a6925a6d14ee21d38643f2611bbb343d9f6aa598
Milan Giri <milan.giri17021999@gmail.com>
2021-02-28 10:58:45 +0545

    insert method created in CrudController

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -9,4 +9,12 @@ class CrudController extends Controller
     public function index(){
         return view('index');
     }
+
+    public function insert(Request $request){
+        $this->validate($request,[
+            'name'=>'required | min:3|max:100',
+            'email'=>'required | email |unique:Users,email',
+            'phone'=>'required | min:10'
+        ]);
+    }
 }
